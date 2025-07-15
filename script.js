d6710e47b3a997a1e1b14de53300ae7d628da05e
mtkache09 <apl5.tkachenko@gmail.com>
2025-07-15 06:18:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -466,6 +466,9 @@ function renderPrizeScroll(caseData, winningGiftCost) {
   // Выбираем его достаточно далеко от начала, чтобы было место для "разгона"
   // и достаточно далеко от конца, чтобы было место для "торможения".
   const targetWinningIndex = 149; // Например, между 75 и 84
+  // ✅ ВСТАВЛЯЕМ ОТЛАДОЧНЫЕ СООБЩЕНИЯ
+  console.log("DEBUG: renderPrizeScroll - Целевой индекс:", targetWinningIndex)
+  console.log("DEBUG: renderPrizeScroll - Ожидаемый приз:", winningGiftCost)
 
   console.log("DEBUG: renderPrizeScroll - Ожидаемый выигрышный приз (winningGiftCost):", winningGiftCost)
   console.log(
