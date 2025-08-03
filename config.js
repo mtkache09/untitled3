58d7366695d7f2a44dd81e7fba3872f3fbc99eee
mtkache09 <apl5.tkachenko@gmail.com>
2025-08-03 16:59:56 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,34 @@
+// Конфигурация приложения
+export const CONFIG = {
+  API_BASE: (() => {
+    if (window.location.hostname === "mtkache09.github.io") {
+      return "https://telegramcases-production.up.railway.app"
+    }
+    return "http://localhost:8000"
+  })(),
+
+  ANIMATION: {
+    SPIN_DURATION: 3000,
+    PRIZE_WIDTH: 128,
+    NOTIFICATION_DURATION: 3000,
+  },
+
+  DEPOSIT_AMOUNTS: [10, 50, 100, 500, 1000, 5000],
+
+  RATE_LIMITS: {
+    MAX_CASES_PER_MINUTE: 10,
+  },
+}
+
+// Глобальное состояние приложения
+export const STATE = {
+  tonConnectUI: null,
+  walletData: null,
+  currentCase: null,
+  isSpinning: false,
+  userFantics: 0,
+  selectedDepositAmount: null,
+  topupPayload: null,
+}
+
+export default { CONFIG, STATE }
